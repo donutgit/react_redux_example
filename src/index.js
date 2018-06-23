7e4941ac7629a526b66ec0af342d4d6502d84b99
donutgit <biutifulzika@gmail.com>
2018-06-23 13:05:14 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from 'redux-thunk';
+
+// import reducer from './store/reducer';
+import counterReducer from "./store/reducers/counter";
+import resultReducer from "./store/reducers/result";
+
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+const reducer = combineReducers({
+  ctr: counterReducer,
+  res: resultReducer
+}); 
+
+const logger = store => {
+  return next => {
+    return action => {
+      console.log("[Middleware] dispatching", action);
+      const result = next(action);
+      console.log("[Middleware] next state", store.getState());
+      return result;
+    };
+  };
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+
+const store = createStore(reducer, enhancer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+registerServiceWorker();
